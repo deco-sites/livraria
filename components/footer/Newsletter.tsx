79b86ea32ed419119bdb1c49375a18372252e29b
Gabriel Leocadio <gabrielsilva94@gmail.com>
2023-03-18 23:27:43 -0300

    feat: estilo e estrutura da newsletter

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -1,27 +1,46 @@
 import Text from "$store/components/ui/Text.tsx";
+import Icon from "$store/components/ui/Icon.tsx";
 
 function Newsletter() {
   return (
-    <div class="flex flex-col  sm:flex-row items-center justify-center gap-6 sm:gap-20">
+    <div class="grid grid-cols-[45%_55%] items-center justify-between">
       <div class="flex items-center justify-between gap-2 bg-newsletter pl-20 pr-10 py-[27px]">
-        <Text variant="heading-2" tone="default-inverse">
-          Fique por dentro das novidades e eventos da Vila!
+        <Text
+          variant="heading-2"
+          tone="default-inverse"
+          class="flex justify-between items-center gap-2"
+        >
+          <Icon
+            id="Mail"
+            size={46}
+          />
+          <div>
+            <p>Fique por dentro das</p>
+            <p>novidades e eventos da Vila!</p>
+          </div>
         </Text>
+
+        <Icon
+          id="DoubleRightArrow"
+          width={22}
+          height={25}
+        />
       </div>
-      <form class="flex flex-row items-center gap-2 font-body text-body w-full">
+
+      <form class="flex flex-row items-center justify-center font-body text-body w-full py-[27px]">
         <input
-          class="py-2 px-3 flex-grow bg-footer rounded text-default border-1 border-default"
+          class="px-3 bg-white rounded text-black border-1 border-default mr-[25px] w-2/5 h-[35px]"
           id="nome"
           placeholder="Digite seu nome"
         />
 
         <input
-          class="py-2 px-3 flex-grow bg-footer rounded text-default border-1 border-default"
+          class="px-3 bg-white rounded text-black border-1 border-default mr-[25px] w-2/5 h-[35px]"
           id="nome"
           placeholder="agora, seu email"
         />
         <button
-          class="py-2 px-3 bg-interactive-inverse rounded bg-newsletter"
+          class="py-2 px-3 bg-interactive-inverse rounded bg-newsletter w-[40px] h-[37px]"
           type="bgutton" // prevent form's default behavior
         >
           Cadastrar
