ef997c44ff9fb6243799db9978b762b1ed486741
Gabriel Pombalino <gabrielpombalino@gmail.com>
2023-03-24 23:51:39 -0300

    feat: adjusts to header mobile

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -16,73 +16,81 @@ function Navbar({ items, searchbar }: {
   return (
     <>
       {/* Mobile Version */}
-      <div
-        class={`md:hidden flex flex-row justify-between items-center h-[${navbarHeight}] border-b-1 border-default w-full px-2 gap-2`}
-      >
-        <HeaderButton variant="menu" />
-
-        <a
-          href="/"
-          class={`flex-grow inline-flex items-center min-h-[${navbarHeight}]`}
-          aria-label="Store logo"
+      <div class="md:hidden bg-white">
+        <div
+          class={`md:hidden flex flex-row justify-between items-center h-[${navbarHeight}]  w-full py-[10px] px-[5px]`}
         >
-          <Icon id="Logo" width={126} height={16} />
-        </a>
+          <HeaderButton variant="menu" />
 
-        <div class="flex gap-1">
-          <HeaderButton variant="search" />
-          <HeaderButton variant="cart" />
+          <a
+            href="/"
+            class={`flex-grow inline-flex items-center min-h-[${navbarHeight}]`}
+            aria-label="Store logo"
+          >
+            <Icon id="Logo" width={190} height={10} />
+          </a>
+
+          <div class="flex gap-1">
+            <HeaderButton variant="user" />
+            <HeaderButton variant="wishlist" />
+            <HeaderButton variant="cart" />
+          </div>
+        </div>
+        <div class="w-[95%] mx-auto py-[10px]">
+          <Searchbar />
         </div>
       </div>
 
       {/* Desktop Version */}
-      <div class="hidden md:flex flex-row justify-center items-center border-b-1 border-default w-full pl-2 pr-3 bg-white">
-        <div class="md:flex flex-row w-[1380px] items-center justify-between py-[10px] px[5px]">
-          <div class="flex justify-center w-[452px]">
-            <a
-              href="/"
-              aria-label="Store logo"
-              class="flex justify-center w-[452px] cursor-pointer"
-            >
-              <Icon id="Logo" width={373} height={23} />
-            </a>
-          </div>
-          <div class="flex-auto flex justify-center">
-            <Searchbar />
-          </div>
-          <div class="flex-none flex items-center justify-end gap-2">
-            <Button
-              as="a"
-              variant="icon"
-              href="/login"
-              aria-label="Log in"
-              class="w-[123px]"
-            >
-              <Icon id="User" width={22} height={19} strokeWidth={0.4} />
-              <Text class="text-[12px] ml-1">Login</Text>
-            </Button>
-            <Button
-              as="a"
-              variant="icon"
-              href="/wishlist"
-              aria-label="wishlist"
-              class="w-[123px]"
-            >
-              <Icon id="Wishlist" width={22} height={19} strokeWidth={0.4} />
-              <Text class="text-[12px] ml-1">Favoritos</Text>
-            </Button>
-            <div class="w-[110px]">
-              <HeaderButton variant="cart" />
+      <div class="hidden lg: block">
+        <div class="hidden md:flex flex-row justify-center items-center border-b-1 border-default w-full pl-2 pr-3 bg-white">
+          <div class="md:flex flex-row w-[1380px] items-center justify-between py-[10px] px[5px]">
+            <div class="flex justify-center w-[452px]">
+              <a
+                href="/"
+                aria-label="Store logo"
+                class="flex justify-center w-[452px] cursor-pointer"
+              >
+                <Icon id="Logo" width={373} height={23} />
+              </a>
+            </div>
+            <div class="flex-auto flex justify-center">
+              <Searchbar />
+            </div>
+            <div class="flex-none flex items-center justify-end gap-2">
+              <Button
+                as="a"
+                variant="icon"
+                href="/login"
+                aria-label="Log in"
+                class="w-[123px]"
+              >
+                <Icon id="User" width={22} height={19} strokeWidth={0.4} />
+                <Text class="text-[12px] ml-1">Login</Text>
+              </Button>
+              <Button
+                as="a"
+                variant="icon"
+                href="/wishlist"
+                aria-label="wishlist"
+                class="w-[123px]"
+              >
+                <Icon id="Wishlist" width={22} height={19} strokeWidth={0.4} />
+                <Text class="text-[12px] ml-1">Favoritos</Text>
+              </Button>
+              <div class="w-[110px]">
+                <HeaderButton variant="cart" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="flex-auto flex justify-center bg-white">
-        <Container class="w-full ">
-          <div class="md: grid grid-flow-col">
-            {items.map((item) => <NavItem item={item} />)}
-          </div>
-        </Container>
+        <div class="lg: flex flex-auto flex justify-center bg-white">
+          <Container class="w-full ">
+            <div class="md: grid grid-flow-col">
+              {items.map((item) => <NavItem item={item} />)}
+            </div>
+          </Container>
+        </div>
       </div>
     </>
   );
